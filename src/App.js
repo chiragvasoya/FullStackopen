fc6307b0b6878727708490a4d28a0030ae0399dd
Chirag <chirag@ledworldlighting.com>
2022-12-01 16:08:35 -0700

    Used js objects instead of variables

--- src/App.js
+++ src/App.js
@@ -14,36 +14,47 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props.parts);
   return (
       <div>
-       <Part part={props.part1} exercise={props.exercise1} />
-       <Part part={props.part2} exercise={props.exercise2} />
-       <Part part={props.part3} exercise={props.exercise3} />
+       <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+       <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+       <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
       </div>
   );
 }
 
 const Total = (props) => {
   return (
-    <p>Number of exercises { props.exercises }</p>
+    <p>Number of exercises { props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises }</p>
   )
 }
 
 const App = () => {
-  const course = "Front End"
-  const part1 = "Fundamental of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of Component"
-  const exercises3 = 14
+  const course = {
+    name: 'Front End',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
  
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} exercise1={exercises1} part2={part2} exercise2={exercises2} part3={part3} exercise3={exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
      
-      <Total exercises={exercises1 + exercises2 + exercises3}  />
+      <Total parts={course.parts}  />
       
       
     </div>
