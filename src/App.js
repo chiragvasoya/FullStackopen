82be38a6c924897bc6b7e6e05989712ac40dfd72
Chirag <chirag@ledworldlighting.com>
2022-12-06 12:38:16 -0700

    Step 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
 import { useState } from 'react';
 
-const Display = ({counter, type}) => <div>{type} Counter: {counter}</div>
+const Display = ({vote, type}) => {
+  if (isNaN(vote)) {
+    return (
+    <div>{type} : 0</div>
+    )
+  } 
+  if (type === 'Positive') {
+    return (
+    <div>{type} : {vote} %</div>
+    )
+  } 
+ return (
+  <div>{type} : {vote}</div>
+ )
+}
 
 const Button = ({value, handleClick}) =>  <button onClick={handleClick}>{value}</button>
 
@@ -21,34 +35,42 @@ const History = ({buttonPress}) => {
 
 const App = () => {
 
-   const [left, setLeft] = useState(0);
-   const [right, setRight] = useState(0);
-   const [allClicks, setAll] = useState([]);
+   const [good, setGood] = useState(0);
+   const [bad, setBad] = useState(0);
+   const [neutral, setNeutral] = useState(0);
+   const [avg, setAvg] = useState(0);
+   const total = good + bad + neutral;
 
-   const leftClick = () => {  
-
-      setLeft(left + 1);
-      setAll([...allClicks, 'L']);
-      // setAll(allClicks.concat('L'));
-   }; 
-
-
-
-   const rightClick = () => { 
-      setRight(right + 1);
-      setAll([...allClicks, 'R']);
-      // setAll(allClicks.concat('R'))
+   const goodClick = () => {
+    setGood(good + 1);
+    setAvg(avg + 1);
    }
-   
-  
- 
+
+   const badClick = () => {
+    setBad(bad + 1);
+    setAvg(avg - 1);
+   }
+
+   const neutralClick = () => {
+    setNeutral(neutral + 1);
+    setAvg(avg);
+   }
+
+
   return (
     <div>
-      <Display counter={left} type="Left"/>
-      <Button handleClick={leftClick} value="Left" />
-      <Button handleClick={rightClick} value="Right" />
-      <Display counter={right} type="Right" />
-      <History buttonPress={allClicks}  />
+      <h2>Feedback App</h2>
+      <Button handleClick={goodClick} value="Good" />
+      <Button handleClick={badClick} value="Bad" />
+      <Button handleClick={neutralClick} value="Neutral" />
+      <h2>Statstics</h2>
+      <Display vote={good} type="Good" />
+      <Display vote={bad} type="Bad" />
+      <Display vote={neutral} type="Neutral" />
+      <Display vote={total} type="All" />
+      <Display vote={avg/total} type="Avg" />
+      <Display vote={good/total} type="Positive" />
+      {/* <History buttonPress={allClicks}  /> */}
     </div>
   );
   }
